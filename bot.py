06e73de825987ad804a649b75b69d81267774b87
Nikita Agafonov <snapdeveloper2@gmail.com>
2024-10-12 16:36:46 +0500

    Created a project

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
-print('hello world')
+import telebot
+
+api_token = input('Введите токен: ')
+
+bot = telebot.TeleBot(api_token)
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, 'Test')
+
+
+print('Bot is running.')
+if __name__ == '__main__':
+    bot.polling(none_stop=True, interval=0)
