5f1fd8df03fdf3ea2278a8bf4ad94eb27ea25aef
Danila <d.tretiakov@innopolis.university>
2025-01-08 22:43:35 +0300

    refactor: reformat cleanup process

--- process.py
+++ process.py
@@ -10,37 +10,41 @@ log = ColorLogger(name="Media").get_logger()
 
 
 def process(file_path: str, ban_words: list[str], sound_name: str):
-    try:
-        extension = file_path[file_path.rindex(".") + 1:].lower()
-        if extension in VIDEO_EXTENSIONS:
-            video = Video(file_path=file_path)
-            completed_audio_path = processor.filter(
-                audio=video.audiotrack,
-                ban_words=ban_words,
-                sound_name=sound_name,
-            )
-            completed_video_path = video.merge(audiotrack_path=completed_audio_path)
+    def __cleanup_video_files():
+        if os.path.exists(completed_audio_path):
             os.remove(completed_audio_path)
+        if os.path.exists(video.file_path):
             os.remove(video.file_path)
+        if os.path.exists(video.audiotrack.file_path):
             os.remove(video.audiotrack.file_path)
+        if os.path.exists(video.audiotrack.converted_audio_path):
             os.remove(video.audiotrack.converted_audio_path)
-            return completed_video_path
 
-        if extension in AUDIO_EXTENSIONS:
-            audio = Audio(file_path=file_path)
-            completed_audio_path = processor.filter(
-                audio=audio,
-                ban_words=ban_words,
-                sound_name=sound_name,
-            )
-            log.debug('Completed path: %s', completed_audio_path)
-            if os.path.isfile(audio.file_path):
-                os.remove(audio.file_path)
-            if os.path.isfile(audio.converted_audio_path):
-                os.remove(audio.converted_audio_path)
-            return completed_audio_path
-        return file_path
+    def __cleanup_audio_files():
+        if os.path.isfile(audio.file_path):
+            os.remove(audio.file_path)
+        if os.path.isfile(audio.converted_audio_path):
+            os.remove(audio.converted_audio_path)
 
-    except Exception as e:
-        log.error(e, exc_info=True)
-        return file_path
+    extension = file_path[file_path.rindex(".") + 1:].lower()
+    if extension in VIDEO_EXTENSIONS:
+        video = Video(file_path=file_path)
+        completed_audio_path = processor.filter(
+            audio=video.audiotrack,
+            ban_words=ban_words,
+            sound_name=sound_name,
+        )
+        completed_video_path = video.merge(audiotrack_path=completed_audio_path)
+        __cleanup_video_files()
+        return completed_video_path
+
+    if extension in AUDIO_EXTENSIONS:
+        audio = Audio(file_path=file_path)
+        completed_audio_path = processor.filter(
+            audio=audio,
+            ban_words=ban_words,
+            sound_name=sound_name,
+        )
+        __cleanup_audio_files()
+        return completed_audio_path
+    return file_path
